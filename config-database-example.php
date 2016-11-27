a84a1558d8954219a6038ad774bcb148a399ee07
Francesco Guidotti <francescoguidotti@likedo.it>
2016-11-27 22:58:50 +0100

    Initial commit

---
+++ config-database-example.php
@@ -0,0 +1,12 @@
+<?php
+/** Il nome del database di WordPress */
+define('DB_NAME', 'Your db name');
+
+/** Nome utente del database MySQL */
+define('DB_USER', 'Your db user');
+
+/** Password del database MySQL */
+define('DB_PASSWORD', 'Your db password');
+
+/** Hostname MySQL  */
+define('DB_HOST', 'localhost');
---
+++ wp-config-example.php
@@ -0,0 +1,92 @@
+<?php
+/**
+ * Il file base di configurazione di WordPress.
+ *
+ * Questo file viene utilizzato, durante l’installazione, dallo script
+ * di creazione di wp-config.php. Non è necessario utilizzarlo solo via
+ * web, è anche possibile copiare questo file in «wp-config.php» e
+ * riempire i valori corretti.
+ *
+ * Questo file definisce le seguenti configurazioni:
+ *
+ * * Impostazioni MySQL
+ * * Prefisso Tabella
+ * * Chiavi Segrete
+ * * ABSPATH
+ *
+ * È possibile trovare ultetriori informazioni visitando la pagina del Codex:
+ *
+ * @link https://codex.wordpress.org/it:Modificare_wp-config.php
+ *
+ * È possibile ottenere le impostazioni per MySQL dal proprio fornitore di hosting.
+ *
+ * @package WordPress
+ */
+
+// ** Impostazioni MySQL - È possibile ottenere queste informazioni dal proprio fornitore di hosting ** //
+if ( file_exists( dirname( __FILE__ ) . '/config-database.php' ) ) {
+	include( dirname( __FILE__ ) . '/config-database.php' );
+} else {
+	echo 'Config Database non presente!';
+	exit;
+}
+
+/** Charset del Database da utilizzare nella creazione delle tabelle. */
+define('DB_CHARSET', 'utf8mb4');
+
+/** Il tipo di Collazione del Database. Da non modificare se non si ha idea di cosa sia. */
+define('DB_COLLATE', '');
+
+/** wp-content dir path */
+define('WP_CONTENT_DIR', dirname( __FILE__ ).'/content');
+
+/** wp-content dir url */
+define('WP_CONTENT_URL', 'http://'.$_SERVER['HTTP_HOST'].'/content' );
+
+/**#@+
+ * Chiavi Univoche di Autenticazione e di Salatura.
+ *
+ * Modificarle con frasi univoche differenti!
+ * È possibile generare tali chiavi utilizzando {@link https://api.wordpress.org/secret-key/1.1/salt/ servizio di chiavi-segrete di WordPress.org}
+ * È possibile cambiare queste chiavi in qualsiasi momento, per invalidare tuttii cookie esistenti. Ciò forzerà tutti gli utenti ad effettuare nuovamente il login.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'Put your unique phrase here');
+define('SECURE_AUTH_KEY',  'Put your unique phrase here');
+define('LOGGED_IN_KEY',    'Put your unique phrase here');
+define('NONCE_KEY',        'Put your unique phrase here');
+define('AUTH_SALT',        'Put your unique phrase here');
+define('SECURE_AUTH_SALT', 'Put your unique phrase here');
+define('LOGGED_IN_SALT',   'Put your unique phrase here');
+define('NONCE_SALT',       'Put your unique phrase here');
+
+/**#@-*/
+
+/**
+ * Prefisso Tabella del Database WordPress.
+ *
+ * È possibile avere installazioni multiple su di un unico database
+ * fornendo a ciascuna installazione un prefisso univoco.
+ * Solo numeri, lettere e sottolineatura!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * Per gli sviluppatori: modalità di debug di WordPress.
+ *
+ * Modificare questa voce a TRUE per abilitare la visualizzazione degli avvisi
+ * durante lo sviluppo.
+ * È fortemente raccomandato agli svilupaptori di temi e plugin di utilizare
+ * WP_DEBUG all’interno dei loro ambienti di sviluppo.
+ */
+define('WP_DEBUG', false);
+
+/* Finito, interrompere le modifiche! Buon blogging. */
+
+/** Path assoluto alla directory di WordPress. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Imposta le variabili di WordPress ed include i file. */
+require_once(ABSPATH . 'wp-settings.php');
